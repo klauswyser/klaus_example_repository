89ee563f03ca2b1f68d4bec2185b6247c5af542c
klaus <klaus.wyser@smhi.se>
2017-11-08 16:20:17 +0100

    add space

--- example.py
+++ example.py
@@ -20,7 +20,7 @@ def get_word_lengths(s):
     """
     # uncomment next line in step 9
 #   return [len(word) for word in s.split()]
-    return None
+    return  None
 
 
 # uncomment this function in step 6
